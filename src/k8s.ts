28e11d2b64389438f01300cda87effa540738c9e
Adam Doussan <acdoussan@Adams-MacBook-Pro.local>
2023-01-15 16:36:16 -0600

    actual node creation implementation

--- src/k8s.ts
+++ src/k8s.ts
@@ -5,7 +5,7 @@ import { ProxmoxProvider } from 'gen/providers/proxmox/provider';
 
 export class K8sStack extends TerraformStack {
   public readonly proxmoxProvider: ProxmoxProvider;
-  public readonly test: Lxc;
+  public readonly nodes: Record<string, Lxc> = {};
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
@@ -16,29 +16,103 @@ export class K8sStack extends TerraformStack {
       pmPassword: process.env.PM_PASSWORD!,
     });
 
-    this.test = new Lxc(this, 'lxc-test', {
-      cores: 2,
-      memory: 2048,
-      rootfs: {
-        size: '20G',
-        storage: 'local',
+    const nodes = {
+      'k8s-ctrl-1': {
+        cores: 2,
+        memory: 2048,
+        network: [
+          {
+            name: "eth0",
+            bridge: "vmbr0",
+            ip: "dhcp",
+            hwaddr: '1E:88:40:19:F2:7B',
+          },
+        ],
+        targetNode: 'hp1',
       },
-      features: {
-        nesting: true,
+      'k8s-ctrl-2': {
+        cores: 2,
+        memory: 2048,
+        network: [
+          {
+            name: "eth0",
+            bridge: "vmbr0",
+            ip: "dhcp",
+            hwaddr: 'DE:FC:7E:CC:24:6B',
+          },
+        ],
+        targetNode: 'hp2',
       },
-      hostname: "terraform-new-container",
-      network: [
-        {
-          name: "eth0",
-          bridge: "vmbr0",
-          ip: "dhcp",
-        }
-      ],
-      ostemplate: "truenas-nfs-hdd:vztmpl/ubuntu-22.10-standard_22.10-1_amd64.tar.zst",
-      password: process.env.PM_LXC_PASSWORD!,
-      targetNode: "r720",
-      unprivileged: true,
-      start: true,
-    });
+      'k8s-ctrl-3': {
+        cores: 2,
+        memory: 2048,
+        network: [
+          {
+            name: "eth0",
+            bridge: "vmbr0",
+            ip: "dhcp",
+            hwaddr: 'D2:B0:2B:08:2B:0E',
+          },
+        ],
+        targetNode: 'r720',
+      },
+      'k8s-wkr-1': {
+        cores: 2,
+        memory: 4096,
+        network: [
+          {
+            name: "eth0",
+            bridge: "vmbr0",
+            ip: "dhcp",
+            hwaddr: 'EA:E3:F9:60:9F:17',
+          },
+        ],
+        targetNode: 'hp1',
+      },
+      'k8s-wkr-2': {
+        cores: 2,
+        memory: 4096,
+        network: [
+          {
+            name: "eth0",
+            bridge: "vmbr0",
+            ip: "dhcp",
+            hwaddr: 'B6:C1:ED:17:AF:39',
+          },
+        ],
+        targetNode: 'hp2',
+      },
+      'k8s-wkr-3': {
+        cores: 16,
+        memory: 1024 * 20, // 20gb
+        network: [
+          {
+            name: "eth0",
+            bridge: "vmbr0",
+            ip: "dhcp",
+            hwaddr: '12:2D:5A:93:08:9C',
+          },
+        ],
+        targetNode: 'r720',
+      },
+    };
+
+    for (const [hostname, config] of Object.entries(nodes)) {
+      this.nodes[hostname] = new Lxc(this, `lxc-${hostname}`, {
+        rootfs: {
+          size: '20G',
+          storage: 'local',
+        },
+        features: {
+          nesting: true,
+        },
+        ostemplate: "truenas-nfs-hdd:vztmpl/ubuntu-22.10-standard_22.10-1_amd64.tar.zst",
+        password: process.env.PM_LXC_PASSWORD!,
+        unprivileged: true,
+        start: true,
+        hostname,
+        ...config,
+      });
+    }
   }
 }
